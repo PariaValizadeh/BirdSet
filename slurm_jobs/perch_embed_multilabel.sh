4d3b272fa512a27592f50a6e4c6574fc2d7ded84
Moritz Richert <mo01010010itz@protonmail.com>
2024-01-04 23:44:51 +0100

    introduce more jobs

---
+++ slurm_jobs/perch_embed_multilabel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=ml_e_p
+#SBATCH --output=embed_perch_ml.log
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=main
+#SBATCH --gres=gpu:1
+#SBATCH --signal=SIGUSR1@90
+
+date;hostname;pwd
+source /mnt/stud/home/mrichert/.zshrc
+conda activate gadme
+cd /mnt/stud/home/mrichert/Projects/GADME-BaselineResults-BA/src
+export CUDA_LAUNCH_BLOCKING=1
+export HYDRA_FULL_ERROR=1
+srun python -u main.py experiment=s_high_sierras_embed_perch_multilabel trainer=single_gpu paths=server
